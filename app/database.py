8aa2868a991d9e55541493f05612bccfa477d12f
Oumuamua <aryanandroid26@gmail.com>
2024-11-27 16:47:30 +0530

    add the voting system and db deployment

--- app/database.py
+++ app/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from . import config
 
 #postgresql://username:password@localhost:port/database_name
-POSTGRES_CONNECTION_URL = """postgresql://postgres:password123@localhost:5432/cs_explorer_db"""
+POSTGRES_CONNECTION_URL = config.settings.db_protocol+config.settings.db_username+ ":" + config.settings.db_password + "@" + config.settings.db_hostname + ":" + str(config.settings.db_port) + "/" + config.settings.db_name
 
 engine = create_engine(POSTGRES_CONNECTION_URL)
 Base = declarative_base()
